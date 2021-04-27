2f5d7b3a0cb1c4d942bd354d07a8f211f91c502a
LitatyHQ <litatyHQ@gmail.com>
2021-04-26 22:12:16 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20210418220533) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.decimal "balance"
+  end
+
 end
